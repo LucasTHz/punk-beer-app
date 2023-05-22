5a71f23de83b5ca02a8f6f98e6e4bdd28021df03
LucasTHz <lucasthomaz58@gmail.com>
2023-05-22 17:51:46 -0300

    implementando as rotas e estilização

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Appbar } from 'react-native-paper';
+
+export function Home({ navigation }) {
+	return (
+		<View>
+			<Text>Historico Screen</Text>
+		</View>
+	);
+}
