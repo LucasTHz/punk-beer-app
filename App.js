e93f48cd53fe55da7f37c48690915515ac50c110
LucasTHz <lucasthomaz58@gmail.com>
2023-06-09 00:27:56 -0300

    fix/ tema claro personalizado não funcionava

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+	DefaultTheme,
+	MD3DarkTheme,
+	MD3LightTheme,
+	Provider as PaperProvider,
+	adaptNavigationTheme,
+} from 'react-native-paper';
 
 import { NavigationContainer } from '@react-navigation/native';
 import RouteNavBar from './src/routes/RouteNavBar';
 import { TelaLogin } from './src/pages/Login';
-import { theme } from './assets/themes/material';
+import { LightScheme } from './assets/themes/LightScheme';
+import { DarkScheme } from './assets/themes/DarkScheme';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useColorScheme } from 'react-native';
+
+const LightTheme = {
+	...MD3LightTheme,
+	colors: LightScheme,
+};
+
+const DarkTheme = {
+	...MD3DarkTheme,
+	colors: DarkScheme,
+};
 
 const Stack = createStackNavigator();
 
 export default function App() {
+	const scheme = useColorScheme();
+	const theme = scheme === 'dark' ? DarkTheme : LightTheme;
 	return (
-		<PaperProvider theme={theme.colors.light}>
+		<PaperProvider theme={theme}>
 			<NavigationContainer>
 				<Stack.Navigator
 					initialRouteName="Login"
