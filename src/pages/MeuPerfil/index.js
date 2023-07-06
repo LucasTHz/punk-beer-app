119b52975f4959610658bb1edac5cfe3a8500fb4
LucasTHz <lucasthomaz58@gmail.com>
2023-07-05 22:44:47 -0300

    melhorando a responsividade

--- src/pages/MeuPerfil/index.js
+++ src/pages/MeuPerfil/index.js
@@ -4,7 +4,7 @@ import { NavBar } from '../../components/NavBar';
 import { useTheme, Text, TextInput, Button } from 'react-native-paper';
 import { style } from './styles';
 import { useAuth } from '../../contexts/auth';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { updateUser } from '../../services/auth';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { format } from 'date-fns';
@@ -59,105 +59,110 @@ export default function MeuPerfil() {
 	return (
 		<>
 			<NavBar title="Meu Perfil" icon="dots-vertical" />
-			<Text variant="titleLarge" style={style.title}>
-				Olá {user.usu_nome}
-			</Text>
-			<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-				<View style={style.container}>
-					<View style={style.centeredView}>
-						{showDialogError && (
-							<DialogError
-								title={error.error}
-								body={error.message}
-								visible={showDialogError}
-								onDismiss={() => setShowDialogError(false)}
-							/>
-						)}
-						{showDialogSuccess && (
-							<DialogSuccess
-								title="Sucesso!"
-								body={messageSuccess}
-								visible={showDialogSuccess}
-								onDismiss={() => setShowDialogSuccess(false)}
-							/>
-						)}
-						<TextInput
-							label="Cpf"
-							value={user.usu_cpf}
-							mode="outlined"
-							disabled={true}
-							style={style.centeredView.input}
-						></TextInput>
-						<TextInput
-							label="Email"
-							onChange={(email) => setEmail(email)}
-							value={email}
-							disabled={true}
-							mode="outlined"
-							style={style.centeredView.input}
-						></TextInput>
-						<TextInput
-							label="Data de nascimento"
-							value={date}
-							mode="outlined"
-							disabled={true}
-							style={style.centeredView.input}
-							forceTextInputFocus={false}
-						></TextInput>
-						<TextInput
-							label="Nome"
-							onChangeText={(name) => setName(name)}
-							value={name}
-							mode="outlined"
-							placeholder="Nome"
-							autoCapitalize="words"
-							style={style.centeredView.input}
-						></TextInput>
-
-						<TextInput
-							label="Senha"
-							onChangeText={(password) => setPassword(password)}
-							value={password}
-							mode="outlined"
-							autoCompleteType="current-password"
-							placeholder="Senha"
-							autoCapitalize="none"
-							secureTextEntry={showPassord}
-							style={style.centeredView.input}
-							right={
-								<TextInput.Icon icon={showPassord ? 'eye' : 'eye-off'} onPress={() => setShowPassord(!showPassord)} />
-							}
-						></TextInput>
-						<TextInput
-							label="Confirmar Senha"
-							onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
-							value={confirmPassword}
-							mode="outlined"
-							autoCompleteType="current-password"
-							placeholder="Confirmar Senha"
-							autoCapitalize="none"
-							secureTextEntry={showConfirmPassord}
-							style={style.centeredView.input}
-							right={
-								<TextInput.Icon
-									icon={showConfirmPassord ? 'eye' : 'eye-off'}
-									onPress={() => setShowConfirmPassord(!showConfirmPassord)}
+			<View style={style.viewTitle}>
+				<Text variant="titleLarge" style={style.title}>
+					Olá {user.usu_nome}
+				</Text>
+			</View>
+			<ScrollView style={style.viewTitle}>
+				<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+					<View style={style.container}>
+						<View style={style.centeredView}>
+							{showDialogError && (
+								<DialogError
+									title={error.error}
+									body={error.message}
+									visible={showDialogError}
+									onDismiss={() => setShowDialogError(false)}
 								/>
-							}
-						></TextInput>
+							)}
+							{showDialogSuccess && (
+								<DialogSuccess
+									title="Sucesso!"
+									body={messageSuccess}
+									visible={showDialogSuccess}
+									onDismiss={() => setShowDialogSuccess(false)}
+									navigation={() => {}}
+								/>
+							)}
+							<TextInput
+								label="Cpf"
+								value={user.usu_cpf}
+								mode="outlined"
+								disabled={true}
+								style={style.centeredView.input}
+							></TextInput>
+							<TextInput
+								label="Email"
+								onChange={(email) => setEmail(email)}
+								value={email}
+								disabled={true}
+								mode="outlined"
+								style={style.centeredView.input}
+							></TextInput>
+							<TextInput
+								label="Data de nascimento"
+								value={date}
+								mode="outlined"
+								disabled={true}
+								style={style.centeredView.input}
+								forceTextInputFocus={false}
+							></TextInput>
+							<TextInput
+								label="Nome"
+								onChangeText={(name) => setName(name)}
+								value={name}
+								mode="outlined"
+								placeholder="Nome"
+								autoCapitalize="words"
+								style={style.centeredView.input}
+							></TextInput>
+
+							<TextInput
+								label="Senha"
+								onChangeText={(password) => setPassword(password)}
+								value={password}
+								mode="outlined"
+								autoCompleteType="current-password"
+								placeholder="Senha"
+								autoCapitalize="none"
+								secureTextEntry={showPassord}
+								style={style.centeredView.input}
+								right={
+									<TextInput.Icon icon={showPassord ? 'eye' : 'eye-off'} onPress={() => setShowPassord(!showPassord)} />
+								}
+							></TextInput>
+							<TextInput
+								label="Confirmar Senha"
+								onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
+								value={confirmPassword}
+								mode="outlined"
+								autoCompleteType="current-password"
+								placeholder="Confirmar Senha"
+								autoCapitalize="none"
+								secureTextEntry={showConfirmPassord}
+								style={style.centeredView.input}
+								right={
+									<TextInput.Icon
+										icon={showConfirmPassord ? 'eye' : 'eye-off'}
+										onPress={() => setShowConfirmPassord(!showConfirmPassord)}
+									/>
+								}
+							></TextInput>
+						</View>
+						<Button mode="contained" onPress={() => handleUpdate()}>
+							Atualizar
+						</Button>
 					</View>
-					<Button mode="contained" onPress={() => handleUpdate()}>
-						Atualizar
-					</Button>
-					<Text variant="labelMedium" style={style.footer}>
-						Caso deseja excluir sua conta,{' '}
-						<TouchableOpacity style={style.buttonDeleteAccount} onPress={handleDelete()}>
-							<Text variant="labelMedium">clique aqui</Text>
-						</TouchableOpacity>
-					</Text>
-					{showDialogError && <DialogError show={showDialogError} title={error.error} body={error.message} />}
-				</View>
-			</TouchableWithoutFeedback>
+				</TouchableWithoutFeedback>
+			</ScrollView>
+			<Text variant="labelMedium" style={style.footer}>
+				Caso deseja excluir sua conta,{' '}
+				<TouchableOpacity style={style.buttonDeleteAccount} onPress={handleDelete()}>
+					<Text variant="labelMedium">clique aqui</Text>
+				</TouchableOpacity>
+			</Text>
+			{showDialogError && <DialogError show={showDialogError} title={error.error} body={error.message} />}
 		</>
 	);
 }
